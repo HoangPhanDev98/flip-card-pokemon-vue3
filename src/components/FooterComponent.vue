5af97a79a6debd3602dc3049c8531611a426ff63
Hoang Phan <hoangphandev98@gmail.com>
2023-08-28 21:28:34 +0700

    Add scss

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <p>
+      This game owned by
+      <a href="https://github.com/HoangPhanDev98">HoangPhanDev98</a>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+
+  a {
+    color: #f4dc26;
+    padding-left: 0.325rem;
+  }
+}
+</style>
--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -1,15 +1,17 @@
 <template>
-  <h1>POKE MEMORIES</h1>
-  <p>Select mode to start game</p>
-  <div class="modes">
-    <button
-      v-for="mode in modeList"
-      :key="mode.totalBlockOfBoard"
-      @click="onSelectMode(mode.totalBlockOfBoard)"
-    >
-      <span>{{ mode.boardSize }}</span>
-      <span>{{ mode.difficulty }}</span>
-    </button>
+  <div class="screen">
+    <h1>POKE MEMORIES</h1>
+    <p>Select mode to start game</p>
+    <div class="modes">
+      <button
+        v-for="mode in modeList"
+        :key="mode.totalBlockOfBoard"
+        @click="onSelectMode(mode.totalBlockOfBoard)"
+      >
+        <span>{{ mode.boardSize }}</span>
+        <span>{{ mode.difficulty }}</span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -45,3 +47,67 @@ const onSelectMode = (totalBlockOfBoard: number) => {
   emit('onStart', totalBlockOfBoard);
 };
 </script>
+
+<style lang="scss" scoped>
+.screen {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: var(--dark);
+  color: var(--light);
+
+  h1 {
+    font-size: 4.5rem;
+    text-transform: uppercase;
+  }
+
+  p {
+    font-size: 2rem;
+  }
+}
+
+.modes {
+  display: flex;
+  margin-top: 2rem;
+
+  button {
+    font: var(--font);
+    width: 150px;
+    height: 150px;
+    background: transparent;
+    box-shadow: none;
+    border: 1px solid var(--light);
+    color: var(--light);
+    display: flex;
+    flex-direction: column;
+    border-radius: 1rem;
+    margin: 0 1rem;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: background 0.3s ease-in-out;
+
+    &:hover {
+      background-color: var(--light);
+      color: var(--dark);
+    }
+
+    span:first-child {
+      font-size: 2rem;
+    }
+
+    span:last-child {
+      display: block;
+      font-size: 1.25rem;
+      margin-top: 0.5rem;
+    }
+  }
+}
+</style>
